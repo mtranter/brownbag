3ac0d77b9d3562efa549cee8c69b43fa733616e0
Mark Tranter <mark.a.tranter@gmail.com>
2016-08-14 13:46:32 +0100

    helloworld2

--- src/main/scala/org/judo/brownbag/01 HelloWorld.scala
+++ src/main/scala/org/judo/brownbag/01 HelloWorld.scala
@@ -7,7 +7,25 @@ object HelloWorld extends App{
 
   override def main(args: Array[String]): Unit = {
 
-    println("Hello, World!")
+    //const string name = GetName(args);
+    val name = getName(args)
 
+    if(name != null){
+      println(s"Hello, $name!")
+    }else{
+      println("Hello no-one!")
+    }
+  }
+
+  def getName(args: Array[String]): String = {
+    //String name = null;
+    var name: String = null
+
+    if(args.length == 1){
+      //name = args[0];
+      name = args(0)
+    }
+    //return name;
+    name
   }
 }
