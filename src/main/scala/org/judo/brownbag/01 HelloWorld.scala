b08399395ca5e7121bea6fe7cd9472547937a7ab
Mark Tranter <mark.a.tranter@gmail.com>
2016-08-14 13:27:04 +0100

    hello world 1

---
+++ src/main/scala/org/judo/brownbag/01 HelloWorld.scala
@@ -0,0 +1,13 @@
+package org.judo.brownbag
+
+/**
+  * Created by mark on 14/08/16.
+  */
+object HelloWorld extends App{
+
+  override def main(args: Array[String]): Unit = {
+
+    println("Hello, World!")
+
+  }
+}
