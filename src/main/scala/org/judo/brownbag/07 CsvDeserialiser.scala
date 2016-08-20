af4b6446c61289379f998f8071a3d015174eeb37
Mark Tranter <mark.a.tranter@gmail.com>
2016-08-20 08:57:49 +0100

    CSV Deserialization

--- src/main/scala/org/judo/brownbag/07 CsvDeserialiser.scala
+++ src/main/scala/org/judo/brownbag/07 CsvDeserialiser.scala
@@ -3,11 +3,9 @@ package org.judo.brownbag
 /**
   * Created by mark on 15/08/16.
   */
-object CsvSerialiser {
-  type CW[T] = CsvTokenWriter[T]
+object CsvDeserialiser {
   type CR[T] = CsvTokenReader[T]
 
-  def writeCsv[T](graph: T)(implicit cw : CW[T]) = cw.write(graph)
   def readCsv[T](csv: String)(implicit cr : CR[T]) = cr.read(csv)
 }
 
@@ -15,27 +13,72 @@ trait CsvTokenReader[T]{
   def read(token: String): T
 }
 
-trait CsvTokenWriter[T]{
-  def write(valToWrite: T): String
-}
 
-trait CsvReaderWriter[T] extends CsvTokenReader[T] with CsvTokenWriter[T]
+trait DefaultFormats {
 
-object DefaultFormats {
-  type RW[T] = CsvReaderWriter[T]
-  implicit val stringRw = ToFromStringReaderWriter((s: String) => s)
-  implicit val intRw = ToFromStringReaderWriter((s: String) => s.toInt)
-  implicit val doubleRw = ToFromStringReaderWriter((s: String) => s.toDouble)
-  implicit val floatRw = ToFromStringReaderWriter((s: String) => s.toFloat)
-  implicit val boolRw = ToFromStringReaderWriter((s: String) => s.toBoolean)
+  type CR[T] = CsvTokenReader[T]
 
-  object ToFromStringReaderWriter{
-    def apply[T](fr: String => T) = new ToFromStringReaderWriter[T](fr)
-    def apply[T](fr: String => T, fw: T => String = (s:T) => s.toString) = new ToFromStringReaderWriter[T](fr, fw)
+  implicit val stringRw = ToFromStringReader((s: String) => s)
+  implicit val intRw = ToFromStringReader((s: String) => s.toInt)
+  implicit val doubleRw = ToFromStringReader((s: String) => s.toDouble)
+  implicit val floatRw = ToFromStringReader((s: String) => s.toFloat)
+  implicit val boolRw = ToFromStringReader((s: String) => s.toBoolean)
+
+  object ToFromStringReader{
+    def apply[T](fr: String => T) = new ToFromStringReader[T](fr)
   }
-  class ToFromStringReaderWriter[T](fr: String => T, fw: T => String = (s: T) => s.toString) extends CsvReaderWriter[T]{
+
+  class ToFromStringReader[T](fr: String => T, fw: T => String = (s: T) => s.toString) extends CsvTokenReader[T]{
     override def read(token: String): T = fr(token)
+  }
 
-    override def write(valToWrite: T): String = fw(valToWrite)
+  implicit def format1[T, P1 : CR](ctr: P1 => T): CsvTokenReader[T] = new CsvTokenReader[T] {
+    override def read(token: String): T = {
+      val p1Reader = implicitly[CsvTokenReader[P1]]
+      val p1 = p1Reader.read(token)
+      ctr(p1)
+    }
+  }
+
+  implicit def format2[T, P1 : CR, P2 : CR](ctr: (P1, P2) => T): CsvTokenReader[T] = new CsvTokenReader[T] {
+    override def read(token: String): T =  {
+      val strings = token split "," map { s => s.trim}
+      val p1Reader = implicitly[CsvTokenReader[P1]]
+      val p1 = p1Reader.read(strings(0))
+      val p2Reader = implicitly[CsvTokenReader[P2]]
+      val p2 = p2Reader.read(strings(1))
+      ctr(p1, p2)
+    }
+  }
+
+  implicit def format3[T, P1 : CR, P2 : CR, P3 : CR](ctr: (P1, P2, P3) => T): CsvTokenReader[T] = new CsvTokenReader[T] {
+    override def read(token: String): T =  {
+      val strings = token split "," map { s => s.trim}
+      val p1Reader = implicitly[CsvTokenReader[P1]]
+      val p1 = p1Reader.read(strings(0))
+      val p2Reader = implicitly[CsvTokenReader[P2]]
+      val p2 = p2Reader.read(strings(1))
+      val p3Reader = implicitly[CsvTokenReader[P3]]
+      val p3 = p3Reader.read(strings(2))
+      ctr(p1, p2, p3)
+    }
+  }
+
+  implicit def format4[T, P1 : CR, P2 : CR, P3 : CR, P4 : CR](ctr: (P1, P2, P3, P4) => T): CsvTokenReader[T] = new CsvTokenReader[T] {
+    override def read(token: String): T =  {
+      val strings = token split "," map { s => s.trim}
+      val p1Reader = implicitly[CsvTokenReader[P1]]
+      val p1 = p1Reader.read(strings(0))
+      val p2Reader = implicitly[CsvTokenReader[P2]]
+      val p2 = p2Reader.read(strings(1))
+      val p3Reader = implicitly[CsvTokenReader[P3]]
+      val p3 = p3Reader.read(strings(2))
+      val p4Reader = implicitly[CsvTokenReader[P4]]
+      val p4 = p4Reader.read(strings(3))
+
+      ctr(p1, p2, p3, p4)
+    }
   }
 }
+
+object DefaultFormats extends DefaultFormats
--- src/test/scala/org/judo/brownbag/CsvWriterSpec.scala
+++ src/test/scala/org/judo/brownbag/CsvWriterSpec.scala
@@ -6,43 +6,61 @@ import org.scalatest.{FlatSpec, Matchers}
   * Created by mark on 15/08/16.
   */
 class CsvWriterSpec extends FlatSpec with Matchers {
-  "The CsvDeserializer" should "correctly serialize a string value given an explicit writer" in {
-    val csvStr = CsvSerialiser.writeCsv("123")(new CsvTokenWriter[String] {
-      override def write(valToWrite: String): String = valToWrite
-    })
-    csvStr should be("123")
-  }
 
-  it should "correctly deserialize a string value given an explicit reader" in {
-    val deserialzedString = CsvSerialiser.readCsv("123")(new CsvTokenReader[String] {
+
+  "The CsvDeserializer" should "correctly deserialize a string value given an explicit reader" in {
+    val deserialzedString = CsvDeserialiser.readCsv("123")(new CsvTokenReader[String] {
       override def read(token: String): String = token
     })
     deserialzedString should be("123")
   }
 
-  it should "correctly serialize a int value given an explicit writer" in {
-    val csvStr = CsvSerialiser.writeCsv(123)(new CsvTokenWriter[Int] {
-      override def write(valToWrite: Int): String = valToWrite.toString
-    })
-    csvStr should be("123")
-  }
 
   it should "correctly deserialize a int value given an explicit reader" in {
-    val deserialzedString = CsvSerialiser.readCsv("123")(new CsvTokenReader[Int] {
+    val deserialzedString = CsvDeserialiser.readCsv("123")(new CsvTokenReader[Int] {
       override def read(token: String): Int = token.toInt
     })
     deserialzedString should be(123)
   }
 
-  it should "correctly serialize a int value given an imported implicit writer" in {
-    import DefaultFormats._
-    val csvStr = CsvSerialiser.writeCsv(123)
-    csvStr should be("123")
-  }
 
   it should "correctly deserialize a int value given an imported implicit reader" in {
     import DefaultFormats._
-    val deserialzedString = CsvSerialiser.readCsv[Int]("123")
+    val deserialzedString = CsvDeserialiser.readCsv[Int]("123")
     deserialzedString should be(123)
   }
+
+  object MyFormats extends DefaultFormats {
+    implicit val nameFormat = format1(Name)
+    implicit val fullNameFmt = format2(FullName)
+    implicit val personFormat = format3(Person)
+    implicit val weightedSexedPersonFormat = format4(WeightedSexedPerson)
+  }
+
+  it should "correctly deserialize a case class arity 1 given imported implicit formats" in {
+    import MyFormats._
+    val deserialzedString = CsvDeserialiser.readCsv[Name]("judo")
+    deserialzedString should be(Name("judo"))
+  }
+
+  it should "correctly deserialize a case class arity 2 given imported implicit formats" in {
+    import MyFormats._
+    val deserialzedString = CsvDeserialiser.readCsv[FullName]("judo, payments")
+    deserialzedString should be(FullName("judo","payments"))
+  }
+  it should "correctly deserialize a case class arity 3 given imported implicit formats" in {
+    import MyFormats._
+    val deserialzedString = CsvDeserialiser.readCsv[Person]("1, judo, payments")
+    deserialzedString should be(Person(1, "judo","payments"))
+  }
+  it should "correctly deserialize a case class arity 4 given imported implicit formats" in {
+    import MyFormats._
+    val deserialzedString = CsvDeserialiser.readCsv[WeightedSexedPerson]("1, judo, 93.254, true")
+    deserialzedString should be(WeightedSexedPerson(1, "judo", 93.254, true))
+  }
 }
+
+case class Name(name: String)
+case class FullName(firstName: String, lastName: String)
+case class Person(id: Int,firstName: String, lastName: String)
+case class WeightedSexedPerson(id: Int, name: String, weight: Double, isMale: Boolean)
