5b5437f31acbd9faefdfa0c953c61f23d42e8e8b
Mark Tranter <mark.a.tranter@gmail.com>
2016-09-07 12:32:46 +0100

    take8

--- src/main/scala/org/judo/brownbag/08 TypeClasses.scala
+++ src/main/scala/org/judo/brownbag/08 TypeClasses.scala
@@ -8,12 +8,35 @@ import java.util.Date
 
 object Calculator{
 
-  type Number = Int
-
-  def add(a: Number, b: Number): Number = a + b
-  def subtract(a: Number, b: Number): Number = a - b
-  def multiply(a: Number, b: Number): Number = a * b
+  def add[Number : NumberLike](a: Number, b: Number): Number = implicitly[NumberLike[Number]].add(a,b)
+  def subtract[Number : NumberLike](a: Number, b: Number): Number = implicitly[NumberLike[Number]].subtract(a,b)
+  def multiply[Number : NumberLike](a: Number, b: Number): Number = implicitly[NumberLike[Number]].multiply(a,b)
 
 }
 
 
+trait NumberLike[T]{
+  def add(a: T, b: T): T
+  def subtract(a: T, b: T): T
+  def multiply(a: T, b: T): T
+
+
+
+
+}
+
+object NumberLikeInt extends NumberLike[Int]{
+  override def add(a: Int, b: Int): Int = a + b
+
+  override def multiply(a: Int, b: Int): Int = a * b
+
+  override def subtract(a: Int, b: Int): Int = a - b
+}
+
+object NumberLikeDouble extends NumberLike[Double]{
+  override def add(a: Double, b: Double): Double = a + b
+
+  override def multiply(a: Double, b: Double): Double = a * b
+
+  override def subtract(a: Double, b: Double): Double = a - b
+}
--- src/test/scala/org/judo/brownbag/08 CalculatorSpec.scala
+++ src/test/scala/org/judo/brownbag/08 CalculatorSpec.scala
@@ -9,15 +9,19 @@ class CalculatorSpec extends FunSpec with Matchers{
   describe("Calculator"){
     describe("Add"){
       it("Should return the correct integer addition"){
+        implicit val numberlike = NumberLikeInt
         val retval = Calculator.add(3,4)
         retval should be(7)
       }
-      /*
+
       it("will shit itself for decimals"){
+        implicit val numberlike = NumberLikeDouble
         val retval = Calculator.add(4d, 5d)
         retval should be(9d)
       }
-      */
+
     }
   }
 }
+
+
